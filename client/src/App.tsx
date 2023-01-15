ae3d88c8b6112ec2c5a451f476a604ec8ba83613
finye <finnantiku@gmail.com>
2023-01-15 15:18:09 +0200

    adds visualization to drones

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,6 +24,43 @@ interface MappedDrone {
   pilot?: Pilot
 }
 
+interface DronesInSightProps {
+  drones?: MappedDrone[]
+}
+
+const DronesInSight = ({ drones }: DronesInSightProps): ReactElement => {
+  if (!drones) {
+    return <div>loading...</div>
+  }
+
+  const scaleBy = 1.25
+  const getRadius = (r: number) => r / 1000 / scaleBy
+  const center = getRadius(250_000);
+
+  return (
+    <svg width={500} height={500}>
+
+      <circle cx={center} cy={center} r={center} fill='none' stroke='#607d8b' />
+      <circle cx={center} cy={center} r={getRadius(100_000)} fill='none' stroke='#ff5722' />
+
+      <circle cx={center} cy={center} r={10} fill='#4CAF50' />
+      <text x={center} y={center} text-anchor="right" alignment-baseline="central" fill="white" fontSize='1rem'>Endangered bird</text>
+
+      {drones.map((drone: MappedDrone) => {
+        return (
+          <circle
+            cx={getRadius(drone.x)}
+            cy={getRadius(drone.y)}
+            r={5}
+            fill={drone.isInsideNoFlyZone ? '#f44336' : '#ffc302'}
+          />
+        )
+      })}
+
+    </svg>
+  )
+}
+
 const DronesApp = () => {
   const [drones, setDrones] = React.useState<MappedDrone[]>();
 
@@ -87,7 +124,7 @@ const DronesApp = () => {
     <div className="App">
       <header className="App-header">
         <div>
-          {/* <canvas ref={canvasRef} width={500} height={500}/> */}
+          <DronesInSight drones={drones} />
         </div>
 
         <Pilots />
